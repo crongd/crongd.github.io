906d5046f92a5f4c83e5158368fd1729bf8af92d
jaeho <118298781+crongd@users.noreply.github.com>
2024-01-24 16:38:23 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 
 document.querySelector('button').onclick = () => {
-    document.body.insertAdjacentHTML('beforeend', '<br><img src="image/istockphoto-104355461-612x612.jpg" alt="고양이 사진"/>')
+    document.body.insertAdjacentHTML('beforeend', '<br><img src="image/main.jpg" alt="고양이 사진"/>')
 }
